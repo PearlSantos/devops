4fbe726eb4cca6891b4556527441f74c3564876b
Pearl Jen Santos <pearl.jen.santos@ing.asia.com>
2018-12-03 11:56:42 +0800

    initial commit for provision vm

---
+++ terraform/provision_vm/main.tf
@@ -0,0 +1,54 @@
+provider "azurerm" {
+  subscription_id = "${var.azure_subscription_id}"
+  client_id       = "${var.azure_client_id}"
+  client_secret   = "${var.azure_client_secret}"
+  tenant_id       = "${var.azure_tenant_id}"
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = "openshiftvnet"
+  resource_group_name = "ingasiabenedict"
+}
+
+data "azurerm_subnet" "subnet" {
+  name                 = "mastersubnet"
+  resource_group_name  = "${data.azurerm_virtual_network.vnet.resource_group_name}"
+  virtual_network_name = "${data.azurerm_virtual_network.vnet.name}"
+}
+
+module "cdaas_vm" {
+  source                = "../modules/vm"
+  azure_subscription_id = "${var.azure_subscription_id}"
+  azure_client_id       = "${var.azure_client_id}"
+  azure_client_secret   = "${var.azure_client_secret}"
+  azure_tenant_id       = "${var.azure_tenant_id}"
+  ssh_key               = "${var.ssh_key}"
+  resource_group_name   = "${module.resource_group.name}"
+  cidr_subnet           = "${var.cidr_subnet}"
+  subnet_id             = "${module.subnet.subnet_id}"
+
+  security_rule = [{
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefixes    = ["${var.ibss_manila_public_ip}", "${var.agent_ip}"]
+    destination_address_prefix = "*"
+  }]
+
+  vm_size           = "Standard_DS3_v2"
+  managed_disk_type = "Standard_LRS"
+
+  virtual_machine_name = "${var.sample_vm_name}"
+  computer_name        = "cdaas-asia-sample-pipeline-vm-test"
+  domain_name_label    = "cdaas-asia-sample-pipeline-vm-test"
+
+  tags = {
+    app               = "cdaas-asia-vm"
+    role              = "sample"
+    shutdown_weekends = "true"
+  }
+}
