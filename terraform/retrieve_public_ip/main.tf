378137d93815fc9b7345ef1bbefa498eacf57ae8
Pearl Jen Santos <pearl.jen.santos@ing.asia.com>
2018-12-03 15:23:27 +0800

    fixed retrieving public IP script

--- terraform/retrieve_public_ip/main.tf
+++ terraform/retrieve_public_ip/main.tf
@@ -1,8 +1,8 @@
-data "azurerm_public_ip" "sample_vm_ip" {
-  name                = "${var.sample_vm_name}0-ip"
+data "azurerm_public_ip" "vm_ip" {
+  name                = "${var.vm_name}0-ip"
   resource_group_name = "${var.resource_group_name}"
 }
 
 output "public_ip_address" {
-  value = "${data.azurerm_public_ip.sample_vm_ip.ip_address}"
+  value = "${data.azurerm_public_ip.vm_ip.ip_address}"
 }
